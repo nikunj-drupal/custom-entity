dfa6754f7a044314450ef2c54fda65fe89893890
Nikunj Shah <nshah-consultant@scholastic.com>
2021-01-12 14:15:47 +0530

    first commit

---
+++ nylotto_drawing/src/Form/DrawingDeleteForm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\nylotto_drawing\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Drawing entities.
+ *
+ * @ingroup nylotto_drawing
+ */
+class DrawingDeleteForm extends ContentEntityDeleteForm
+{
+}
